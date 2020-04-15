95ba6134b534138b9776ac1e695c9c60fe5bfa41
Sara <dobosarevic.e246.2019@uns.ac.rs>
2020-04-15 22:56:47 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  prevArrow: $('.arrow-prev'),
+  nextArrow: $('.arrow-next')
+});
+
